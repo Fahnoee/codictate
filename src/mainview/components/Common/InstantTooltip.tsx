dc1e8782eb7428187b14735e4e4dfb0cc7db2fd9
Emil Lykke Grann <emillykkeg@gmail.com>
2026-04-02 10:24:35 +0200

    Bigger tooltip text

--- src/mainview/components/Common/InstantTooltip.tsx
+++ src/mainview/components/Common/InstantTooltip.tsx
@@ -29,7 +29,7 @@ export function InstantTooltip({
       {children}
       <span
         role="tooltip"
-        className={`pointer-events-none absolute ${positionClass} z-60 w-[min(100vw-2rem,15rem)] whitespace-normal rounded-lg border border-white/14 bg-[#1c1c1f]/98 px-3 py-2 text-left text-[13px] leading-snug text-white/90 shadow-lg opacity-0 transition-none group-hover:opacity-100 group-focus-within:opacity-100`}
+        className={`pointer-events-none absolute ${positionClass} z-60 w-[min(100vw-2rem,15rem)] whitespace-normal rounded-lg border border-white/14 bg-[#1c1c1f]/98 px-3 py-2 text-left text-[18px] leading-snug text-white/90 shadow-lg opacity-0 transition-none group-hover:opacity-100 group-focus-within:opacity-100`}
       >
         {text}
       </span>
